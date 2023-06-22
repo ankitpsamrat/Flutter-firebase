b3c70d027cb17ae6bd63aea99bfe39b05e000f77
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-06-22 23:08:01 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fire/config/firebase_options.dart';
-import 'package:flutter_fire/screens/sign_in_screen.dart';
+import 'package:flutter_fire/src/screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: SignInScreen(),
+      home: HomeScreen(),
     );
   }
 }
