a72b6cb4bb44555d61929b2cd57134f4f35f16b1
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-07-08 23:52:28 +0530

    changes

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,86 +1,11 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_fire/services/firebase_service.dart';
-// import 'package:flutter_fire/src/screens/add_bio_screen.dart';
-// import 'package:flutter_fire/src/screens/sign_in_screen.dart';
-
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({super.key});
-
-//   @override
-//   State<HomeScreen> createState() => _HomeScreenState();
-// }
-
-// class _HomeScreenState extends State<HomeScreen> {
-//   //
-
-//   // final textController = TextEditingController();
-//   // final formKey = GlobalKey<FormState>();
-
-//   // @override
-//   // void dispose() {
-//   //   textController.dispose();
-//   //   super.dispose();
-//   // }
-
-//   @override
-//   void initState() {
-//     // FirebaseService.fetchBioFromFirestoreDB();
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         automaticallyImplyLeading: false,
-//         elevation: 0,
-//         title: Text('Home Page'),
-//         actions: [
-//           Row(
-//             children: [
-//               ElevatedButton(
-//                 onPressed: () {
-//                   FirebaseService.logOutMethod();
-
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(
-//                       builder: (context) => SignInScreen(),
-//                     ),
-//                   );
-//                 },
-//                 child: Text('Logout'),
-//               ),
-//               SizedBox(width: 20),
-//             ],
-//           ),
-//         ],
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           Navigator.push(
-//             context,
-//             MaterialPageRoute(
-//               builder: (context) => AddBioScreen(),
-//             ),
-//           );
-//         },
-//         child: Icon(Icons.edit),
-//       ),
-//       body: Center(
-//         child: Text('Your bio :'),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_fire/src/screens/add_bio_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  final String userId;
+  // final String userId;
 
-  const HomeScreen({required this.userId});
+  // const HomeScreen({required this.userId});
 
   @override
   Widget build(BuildContext context) {
@@ -91,7 +16,7 @@ class HomeScreen extends StatelessWidget {
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
             .collection('userBio')
-            .where('userId', isEqualTo: userId)
+            // .where('userId', isEqualTo: userId)
             .snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -106,8 +31,8 @@ class HomeScreen extends StatelessWidget {
           }
           if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
             return Center(
-              child: Text('No bio found for user: $userId'),
-            );
+                // child: Text('No bio found for user: $userId'),
+                );
           }
 
           final bioData =
@@ -117,6 +42,17 @@ class HomeScreen extends StatelessWidget {
             child: Text('Bio: ${bioData['bio']}'),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddBioScreen(),
+            ),
+          );
+        },
+        child: Icon(Icons.edit),
       ),
     );
   }
