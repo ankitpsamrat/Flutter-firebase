94bc1c86be6bb7f178a87d86f10c1e7d689c0914
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-07-08 23:52:17 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
       home: (user == null)
           ? SignInScreen()
           : HomeScreen(
-              userId: '',
+              // userId: '',
             ),
     );
   }
