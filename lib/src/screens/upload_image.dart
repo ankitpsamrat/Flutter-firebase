b0b03303d89d6ddad0520d87a816b4c58e7ecd40
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-08-08 23:31:47 +0530

    pick image method

--- lib/src/screens/upload_image.dart
+++ lib/src/screens/upload_image.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_fire/src/widgets/button_widget.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadImage extends StatefulWidget {
   const UploadImage({super.key});
@@ -8,6 +12,27 @@ class UploadImage extends StatefulWidget {
 }
 
 class _UploadImageState extends State<UploadImage> {
+  //
+  File? image;
+
+  Future pickImgFromGallery() async {
+    try {
+      final pickedImage = await ImagePicker().pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 85,
+      );
+
+      if (pickedImage == null) return;
+      final imagePath = File(pickedImage.path);
+
+      setState(() {
+        image = imagePath;
+      });
+    } catch (e) {
+      debugPrint('Unable to pick image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +41,38 @@ class _UploadImageState extends State<UploadImage> {
         centerTitle: true,
         backgroundColor: Colors.teal,
         title: Text('Upload image'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: InkWell(
+              onTap: () {
+                pickImgFromGallery();
+              },
+              child: Container(
+                height: 300,
+                width: 300,
+                margin: EdgeInsets.only(bottom: 40),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                ),
+                child: (image != null)
+                    ? Image.file(
+                        image!,
+                        fit: BoxFit.cover,
+                      )
+                    : Icon(Icons.image),
+              ),
+            ),
+          ),
+          CustomButton(
+            onPressed: () {},
+            btnName: 'Upload image',
+          ),
+        ],
       ),
     );
   }
